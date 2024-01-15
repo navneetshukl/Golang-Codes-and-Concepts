080aa7506dc988387a6c9b3110f960229ccf058a
Navneet Shukla <73691517+navneetshukl@users.noreply.github.com>
2024-01-15 11:03:24 +0000

    Written algorithm for quicksort

--- Sorting/quickSort/main.go
+++ Sorting/quickSort/main.go
@@ -1,1 +1,37 @@
 package main
+
+import "fmt"
+
+func Partition(st, en int, arr []int) int {
+	i, j := st-1, st
+	pivot := arr[en]
+
+	for j < en {
+		if arr[j] < pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+		j++
+	}
+	i++
+	arr[i], arr[en] = arr[en], arr[i]
+	return i
+}
+
+func QuickSort(st, en int, arr []int) {
+	if st < en {
+		parIdx := Partition(st, en, arr)
+		QuickSort(st, parIdx-1, arr)
+		QuickSort(parIdx+1, en, arr)
+	}
+
+}
+
+func main() {
+	arr := []int{5, 4, 7, 8, 7, 7, 2, 1, 4, 6, 8, 64, 6, 5, 8, 7, 3, 1, 4, 16, 5}
+
+	en := len(arr)
+	QuickSort(0, en-1, arr)
+	fmt.Println("Sorted array is")
+	fmt.Println(arr)
+}
